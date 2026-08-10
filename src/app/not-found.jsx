7940eee7e4b75d096b7b8d1114b17f79bffd55f7
Pedro Escapalete <pedroescapalete@gmail.com>
2026-08-10 09:14:26 -0300

    ft: Finaliza a page not-found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from "../app/not-found.module.css";
+
+export default function NotFound() {
+    return (
+        <main className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>404</h1>
+                <h2 className={styles.subtitle}>Page not Found</h2>
+                <p className={styles.text}>This page doesn't exist or may have been moved.</p>
+
+                <div className={styles.buttons}>
+                    <Link href='/'>Home</Link>
+                </div>
+            </div>
+        </main>
+    )
+}
